3d0d936c3745610e7212644f055bab391f1a9537
asucra <146820215+asucra@users.noreply.github.com>
2024-06-13 15:58:31 +0900

    ログイン機能の作成、ヘッダーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  def after_sign_up_path_for(resource)
+    :homes
+  end
+
   def after_sign_in_path_for(resource)
     :homes
   end
