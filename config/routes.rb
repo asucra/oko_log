334dd6860db041c5a2d7f33bfbab63722d2baab8
asucra <146820215+asucra@users.noreply.github.com>
2024-06-21 14:06:48 +0900

    ユーザー編集機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   root to: "welcomes#index"
   resources :anger_logs, only: %i[index new create edit update destroy]
   get "homes" => "homes#index"
