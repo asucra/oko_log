d34503b5b89fe364217e6104528a6a050fe5c898
asucra <146820215+asucra@users.noreply.github.com>
2024-06-23 20:51:46 +0900

    怒り以外の感情の記録機能の作成

--- app/models/anger_log_emotion.rb
+++ app/models/anger_log_emotion.rb
@@ -1,4 +1,6 @@
 class AngerLogEmotion < ApplicationRecord
   belongs_to :anger_log
   belongs_to :emotion
+
+  validates :anger_log_id, uniqueness: { scope: :emotion_id }
 end
--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -1,2 +1,6 @@
 class Emotion < ApplicationRecord
+  has_many :anger_log_emotions, dependent: :destroy
+  has_many :anger_logs, through: :anger_log_emotions
+
+  validates :name, presence: true, uniqueness: true
 end
