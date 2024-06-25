14732cdef508c41edf27a1535098cfbb1cd74eee
asucra <146820215+asucra@users.noreply.github.com>
2024-06-25 14:43:55 +0900

    トップページにリダイレクト設定の追加

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,5 +1,16 @@
 class WelcomesController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :redirect_to_homes_index
+
 
   def index; end
+
+  private
+
+  def redirect_to_homes_index
+    if user_signed_in?
+      redirect_to homes_path, danger: 'ログイン済みです'
+    end
+  end
+
 end
