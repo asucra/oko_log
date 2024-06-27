547c8b98fe1f46bdde0fd761081c51cccd10040b
asucra <146820215+asucra@users.noreply.github.com>
2024-06-27 18:07:46 +0900

    ログイン後ホーム画面とアンガーログ機能画面の編集

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,7 @@
 class HomesController < ApplicationController
-  def index; end
+  def index
+    to = Time.current.end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @anger_logs = current_user.anger_logs.includes(:tags, :emotions).where(occurrence_at: from...to).order(occurrence_at: :desc)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def set_required_label(model, attribute)
+    label_name = t("activerecord.attributes.#{model}.#{attribute}")
+    "#{label_name}  ※"
+  end
 end
